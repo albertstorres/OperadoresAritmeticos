4b8c8d621ae88808396566d92dba55443dfe77b7
Albert Sevy <albertstorres@gmail.com>
2024-01-19 19:55:33 -0300

    Repositório para continuar a partir da aula de operadores aritméticos.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Operadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 25 + 22;
+            int subtracao = 25 - 22;
+            float divisao = 22 / 5;
+            int multiplicacao = 25 * 22;
+
+            Console.WriteLine(soma);
+            Console.WriteLine(subtracao);
+            Console.WriteLine(divisao);
+            Console.WriteLine(multiplicacao);
+        }
+    }
+}
